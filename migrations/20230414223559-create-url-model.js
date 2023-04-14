56de6d6209835825a1fa36a2e2e77849006547b9
seun awonugba <86803598+seunAwonugba@users.noreply.github.com>
2023-04-14 23:50:27 +0100

    updated db model to include no of clicks

--- migrations/20230414223559-create-url-model.js
+++ migrations/20230414223559-create-url-model.js
@@ -1,34 +1,44 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('UrlModels', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      urlCode: {
-        type: Sequelize.STRING
-      },
-      originalUrl: {
-        type: Sequelize.STRING
-      },
-      shortUrl: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('UrlModels');
-  }
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable("UrlModels", {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER,
+            },
+            urlCode: {
+                type: Sequelize.STRING,
+            },
+            originalUrl: {
+                type: Sequelize.STRING,
+                allowNull: false,
+                validate: {
+                    notEmpty: {
+                        msg: "Original url is required",
+                    },
+                },
+            },
+            shortUrl: {
+                type: Sequelize.STRING,
+            },
+            clicks: {
+                type: Sequelize.INTEGER,
+                defaultValue: 0,
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+        });
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable("UrlModels");
+    },
 };
--- models/urlmodel.js
+++ models/urlmodel.js
@@ -1,25 +1,40 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class UrlModel extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+    class UrlModel extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
     }
-  }
-  UrlModel.init({
-    urlCode: DataTypes.STRING,
-    originalUrl: DataTypes.STRING,
-    shortUrl: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'UrlModel',
-  });
-  return UrlModel;
+    UrlModel.init(
+        {
+            urlCode: {
+                type: DataTypes.STRING,
+            },
+            originalUrl: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    notEmpty: {
+                        msg: "Original url is required",
+                    },
+                },
+            },
+            shortUrl: DataTypes.STRING,
+            clicks: {
+                type: DataTypes.INTEGER,
+                defaultValue: 0,
+            },
+        },
+        {
+            sequelize,
+            modelName: "UrlModel",
+        }
+    );
+    return UrlModel;
 };
