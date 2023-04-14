932987f86d4c9cc4775acd2a90cbdaf82264640b
seun awonugba <86803598+seunAwonugba@users.noreply.github.com>
2023-04-14 23:14:34 +0100

    added error middleware

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const express = require("express");
 require("dotenv").config();
 const { StatusCodes, ReasonPhrases } = require("http-status-codes");
+const { errorMiddleware } = require("./middleware");
 const app = express();
 const port = process.env.PORT || 8000;
 const host = "localhost";
 
+app.use(express.json());
 app.get("/", (req, res) => {
     return res.status(StatusCodes.OK).json({
         success: true,
         data: ReasonPhrases.OK,
     });
 });
+
+app.use(errorMiddleware);
 
 app.listen(port, host, () => {
     console.log(`Server is listening on http://${host}:${port}`);
---
+++ middleware/errorMiddleware.js
@@ -0,0 +1,28 @@
+const { CustomErrorHandler } = require("../errors");
+const { StatusCodes } = require("http-status-codes");
+
+const errorMiddleware = (err, req, res, next) => {
+    if (err instanceof CustomErrorHandler) {
+        console.log(`custom error middleware -> ${err}`);
+        return res.status(err.statusCode).json({
+            success: false,
+            data: err.message,
+        });
+    }
+
+    console.log(`Sequelize error -> ${err}`);
+    if (err.name === "SequelizeUniqueConstraintError") {
+        return res.status(StatusCodes.CONFLICT).json({
+            success: false,
+            data: err.errors[0].message,
+        });
+    }
+
+    console.log(`server error middleware -> ${err}`);
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+        success: false,
+        data: err.message.split(":")[2] || "Unknown error occurred",
+    });
+};
+
+module.exports = { errorMiddleware };
